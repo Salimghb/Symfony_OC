8d299754ca0199cbf0387b97aca8fdf117a2b041
Salim Ghbabra <salim.ghbabra@etu.univ-orleans.fr>
2017-03-08 13:31:49 +0100

    zeffzfe

--- src/Salim/PlateformeBundle/Controller/AdvertController.php
+++ src/Salim/PlateformeBundle/Controller/AdvertController.php
@@ -101,90 +101,48 @@ class AdvertController extends Controller
 	public function addAction(Request $request)
 	{
 
-		// On récupère l'Entity Manager (EM)
-		$em = $this->getDoctrine()->getManager();
-
-
-		/*
-		// On crée l'entité à ajouter
-		// Date et id définis automatiquement par le constructeur
-		// Published est déjà défini à true
+	// On crée un objet Advert
 		$advert = new Advert();
-		$advert -> setTitle('Recherche développeur Symfony');
-		$advert -> setAuthor('Salim Ghbabra');
-		$advert -> setContent('Nous recherchons un développeur Symfony débutant sur Orléans...');
+    // J'ai raccourci cette partie, car c'est plus rapide à écrire !
+		$form = $this
+		-> get('form.factory')
+		-> createBuilder(FormType::class, $advert)
+		-> add('date',      DateType::class)
+		-> add('title',     TextType::class)
+		-> add('content',   TextareaType::class)
+		-> add('author',    TextType::class)
+		-> add('published', CheckboxType::class, array('required' => false))
+		-> add('save',      SubmitType::class)
+		-> getForm()
+		;
 
-		// On crée les candidatures
-		$application1 = new Application();
-		$application1->setAuthor('Marine');
-		$application1->setContent("J'ai toutes les qualités requises.");
-
-		$application2 = new Application();
-		$application2->setAuthor('Pierre');
-		$application2->setContent("Je suis très motivé.");
-		
-		// On lie les candidatures à l'annonce
-		$application1->setAdvert($advert);
-		$application2->setAdvert($advert);
-
-		//On récupère toutes les compétences disponibles
-		$listSkills = $em -> getRepository('SalimPlateformeBundle:Skill')->findAll();
-
-		//Pour chaque compétence
-		foreach ($listSkills as $skill) {
-
-			// On crée une relation Annonce-Compétence
-			$advertSkill = new AdvertSkill();
-
-			//On la lie à l'annonce générale
-			$advertSkill -> setAdvert($advert);
-
-			//On la lie à la compétence 
-			//qui change à chaque itération de la boucle
-			$advertSkill -> setSkill($skill);
-
-			//Toutes les compétences sont requises au niveau Expert
-			$advertSkill -> setLevel("Expert");
-
-			//On persiste chaque advertSkill créé
-			$em -> persist($advertSkill);
-
-		}
-
-
-		// Etape 1 : On persiste l'entité 
-		// Elle est maintenant gérée par Doctrine
-		$em -> persist($advert);
-
-		// Etape 1 bis : Aucune cascade n'est définie car 
-		// La relation est définie dans l'entité Application, 
-		// L'entité Advert ne sait pas qu'elle est liée à deux Application. 
-		// On doit tout persister à la main dans ce cas
-		$em -> persist($application1);
-		$em -> persist($application2);
-
-
-		// Etape 2 : On flush tout ce qui a été persisté avant 
-		// Doctrine enregistre dans la base les entités persistées
-		$em -> flush();
-		*/
-
-
+    // Si la requête est en POST
 		if ($request->isMethod('POST')) {
-			$request 
-			-> getSession() 
-			-> getFlashBag() 
-			-> add('notice', 'Annonce bien enregistrée.');
+    // On fait le lien Requête <-> Formulaire
+    // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+			$form->handleRequest($request);
 
-      		// Redirection vers la page de l'annonce ajoutée
-			return $this 
-			-> redirectToRoute('oc_platform_view', 
-				array('id' => $advert -> getId()));
+    // On vérifie que les valeurs entrées sont correctes
+    // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+			if ($form->isValid()) {
+    // On enregistre notre objet $advert dans la base de données, par exemple
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($advert);
+				$em->flush();
+
+				$request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+    // On redirige vers la page de visualisation de l'annonce nouvellement créée
+				return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+			}
 		}
 
-    	// Si on n'est pas en POST, alors on affiche le formulaire
-		return $this 
-		-> render('SalimPlateformeBundle:Advert:add.html.twig');
+    // À ce stade, le formulaire n'est pas valide car :
+    // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+    // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+		return $this->render('SalimPlateformeBundle:Advert:add.html.twig', array(
+			'form' => $form->createView(),
+			));
 
 	}
 
